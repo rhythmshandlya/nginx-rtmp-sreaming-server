3f176f02dc8e32eecdcf5e4ca97662aaf88bf340
Rhythm Shandlya <rhythm@filterpixel.com>
2024-03-17 12:29:04 +0530

    0.0.1-nx

--- apps/md-site/theme.config.jsx
+++
@@ -1,7 +0,0 @@
-export default {
-  logo: <span>Livesync</span>,
-  project: {
-    link: 'https://github.com/rhythmshandlya/nginx-rtmp-sreaming-server'
-  }
-  // ... other theme options
-}
---
+++ packages/hls-player/src/lib/hls-player.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable-next-line */
+export interface HlsPlayerProps {}
+
+export function HlsPlayer(props: HlsPlayerProps) {
+  return (
+      <h1 className="bg-red-900"></h1>
+  );
+}
+
+export default HlsPlayer;
